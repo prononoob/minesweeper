05ca7a1480a5b7381ba7a23b8f0f0ac39bb887b2
prononoob <lollel60606@gmail.com>
2023-02-14 09:04:33 +0100

    Clicking on a space shows if it has a bomb

--- minesweeper.py
+++ minesweeper.py
@@ -26,27 +26,64 @@ class Data:
 		for i in self.bombCoordinates:
 			self.board[i[0]][i[1]] = 1
 
+		for i in self.board:
+			print(i)
+
+
+class ButtonProperties:
+	def __init__(self, sRoot, x, y):
+		self.x, self.y, self.sRoot = x, y, sRoot
+		Button(self.sRoot.root, text=str('%sx%s'%(self.y, self.x)), height = 1, width = 1, command=self.getVal).grid(column=self.x, row=self.y)
+
+	def getVal(self):
+		print(self.y, 'x', self.x, '=', self.sRoot.data.board[self.y][self.x])
 
 class Window:
-	def __init__(self, board, m, n):
-		self.board = board
-		self.m, self.n = m, n
+	def __init__(self):
 		self.root = Tk()
-		self.root.geometry('700x700')
-		self.frm = ttk.Frame(self.root, padding=10)
-		self.createButtons(self.m, self.n)
+		self.root.geometry('100x150')
+		self.chooseDifficulty()
 		self.root.mainloop()
 
 
-	def createButtons(self, height, width):
-		for i in range(width):
-			for j in range(height):
-				Button(self.root, text='B', height = 1, width = 1).grid(column=i, row=j)
+	def chooseDifficulty(self):
+		self.easy = Button(self.root, text='Easy', height=2, width=7, command=lambda: self.createButtons(8, 8, 10))
+		self.easy.grid(column=1, row=1, padx=11, pady=5)
+		self.medium = Button(self.root, text='Medium', height=2, width=7, command=lambda: self.createButtons(16, 16, 40))
+		self.medium.grid(column=1, row=2, pady=5)
+		self.hard = Button(self.root, text='Hard', height=2, width=7, command=lambda: self.createButtons(16, 30, 99))
+		self.hard.grid(column=1, row=3)
+
+
+	def destroySelfMenuButtons(self):
+		self.easy.destroy()
+		self.medium.destroy()
+		self.hard.destroy()
+
+
+	def createButtons(self, height, width, bombs):
+		self.height, self.width, self.bombs = height, width, bombs
+
+		# Set screen size fitting to the difficulty
+		if self.height == 8 and self.width == 8 and self.bombs == 10:
+			self.root.geometry('280x225')
+			self.destroySelfMenuButtons()
+		elif self.height == 16 and self.width == 16 and self.bombs == 40:
+			self.root.geometry('565x450')
+			self.destroySelfMenuButtons()
+		elif self.height == 16 and self.width == 30 and self.bombs == 99:
+			self.root.geometry('1055x450')
+			self.destroySelfMenuButtons()
+
+		self.data = Data(self.height, self.width, self.bombs)
+		for i in range(self.width):
+			for j in range(self.height):
+				globals()['buttonprops%s%s' %(i, j)] = ButtonProperties(self, i, j)
 
 
 if __name__ == '__main__':
-	data = Data(10, 10, 25)
-	for i in data.board:
-		print(i)
-	print(data.bombCoordinates)
-	window = Window(data.board, 5, 10)
+	# data = Data(10, 10, 25)
+	# for i in data.board:
+	# 	print(i)
+	# print(data.bombCoordinates)
+	window = Window()
